ec11d08ee2ce0fbd153febb685fa040ebd487faa
casyfill <casyfill@gmail.com>
2015-09-24 19:54:14 -0400

    Assignment 2

--- README.md
+++ README.md
@@ -8,6 +8,23 @@ I use *[requests](http://docs.python-requests.org/)* module instead of urllib. I
 
 ####Info
 
-This script prints all active (displayed through API) buses for specified route, as well as location for each of them
+This script prints all active (displayed through API) buses for specified route, as well as location for each
+
+to activate enter this:
+	**python show_bus_locations.py xxxx-xxxx-xxxx-xxxx-xxxx B52**
+
+
+##Assignment 2: get_bus_info.py
+
+####Dependencies:
+I use *[requests](http://docs.python-requests.org/)* module instead of urllib. It can be easily installed via *pip*
+
+
+This script prints all active (displayed through API) buses for specified route, as well as location and next station for each, and write following info to .csv file: lon, lat, stop name, distance to stop (in stops)
+
+to activate enter this:
+	**python get_bus_info.py xxxx-xxxx-xxxx-xxxx-xxxx M7 M7.csv**
+
+
 
 
--- get_bus_info.py
+++ get_bus_info.py
@@ -4,29 +4,53 @@
 
 import sys
 import requests
+import csv
 
-KEY = 'b90eda14-6a5f-44d9-8b9a-adb8b507d248'
 
 def getMTAdetailed(key, route, path):
-	'''prints bust route
-	info from NY MTA API'''
-	
-	link = 'http://bustime.mta.info/api/siri/vehicle-monitoring.json'
-	payload = {'key':key, 'VehicleMonitoringDetailLevel':'calls', 'LineRef':route }
-	
-	answer  = requests.get(link, params=payload).json()
-	
-	Active_buses = answer['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    '''prints bust route
+    info from NY MTA API'''
 
-	print '\nBus Line : %s' % route
-	print 'Number of Active Buses : %d' % len(Active_buses)
-	
-	for i, bus in enumerate(Active_buses):
-		lat = bus['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-		lon = bus['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-		print 'Bus %d is at latitude %f and longitude %f' % (i, lat, lon)
+    link = 'http://bustime.mta.info/api/siri/vehicle-monitoring.json'
+    payload = {
+        'key': key, 'VehicleMonitoringDetailLevel': 'calls', 'LineRef': route}
 
+    answer = requests.get(link, params=payload).json()
+
+    Active_buses = answer['Siri']['ServiceDelivery'][
+        'VehicleMonitoringDelivery'][0]['VehicleActivity']
+
+    # print '\nBus Line : %s' % route
+    # print 'Number of Active Buses : %d' % len(Active_buses)
+
+    rows = []  # list for each bus dictionary
+
+    for bus in Active_buses:
+        lat = bus['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+        lon = bus['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+        stop = bus['MonitoredVehicleJourney'][
+            "OnwardCalls"]["OnwardCall"][0]['StopPointName']
+        stopStatus = bus['MonitoredVehicleJourney']["OnwardCalls"][
+            "OnwardCall"][0]['Extensions']['Distances']['PresentableDistance']
+
+        busInfo = {'Latitude': lat, "Longitude": lon,
+                   'Stop Name': stop, 'Stop Status': stopStatus}
+        rows.append(busInfo)
+        # print 'Bus %d is at latitude %f and longitude %f' % (i, lat, lon)
+
+    headersList = ['Latitude', 'Longitude', 'Stop Name', 'Stop Status']
+
+    with open(path, 'wb') as writeFile:
+        wD = csv.DictWriter(
+            writeFile, headersList, restval='', extrasaction='raise', dialect='excel')
+        wD.writeheader()
+
+        for row in rows:
+            wD.writerow(row)
+
+
+# getMTAdetailed(KEY, 'B52', 'p')
 if __name__ == '__main__':
-	getMTA(sys.argv[1],sys.argv[2], sys.argv[3] )
+    getMTAdetailed(sys.argv[1], sys.argv[2], sys.argv[3])
 
-# getMTA(KEY, 'B52')
+
